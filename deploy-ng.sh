30eca0339e4f82614c268155744057c47ada2163
Florian <florian@arista.com>
2021-02-22 15:37:54 +0100

    add kickstart functions

--- deploy-ng.sh
+++ deploy-ng.sh
@@ -3,7 +3,7 @@
 # ./deploy.sh \
 #   --network
 #   --host-nic <Host NIC of the hypervisor> \
-#   --host-ip <Host IP of the hypervisor - only required if '--host_nic' and '--libvirt_nic are the same'> \
+#   --host-ip <Host IP + CIDR of the hypervisor - only required if '--host_nic' and '--libvirt_nic are the same'> \
 #   --host-gw <Gateway of the hypervisor - only required if '--host_nic' and '--libvirt_nic are the same'> \
 #   --host-dns <DNS server of the hypervisor - only required if '--host_nic' and '--libvirt_nic are the same'> \
 #   --host-vlan <Host VLAN (tagged) on Host NIC - optional>
@@ -145,7 +145,10 @@ while (( "$#" )); do
                 exit 1
             fi
             ;;
-
+        --assume-yes)
+                ASSUME_YES=1
+                shift
+                ;;
         -*|--*=) # unsupported flags
             echo "Error: Unsupported flag $1" >&2
             exit 1
@@ -176,7 +179,7 @@ function distro_check {
                 if [ -f "/usr/bin/nmcli" ]; then
                     DETECTED_METHOD=nmcli
                 else
-                    echo "[DEPLOYER] Unsupported network configuration method for  $DISTRO $VERSION)" 
+                    echo "[DEPLOYER] Unsupported network configuration method for $DISTRO $VERSION)" 
                 fi
             else
                 echo "[DEPLOYER] Unsupported distro version (Detected $DISTRO $VERSION)"
@@ -199,7 +202,7 @@ function init {
 }
 
 function deps_rhel8 {
-    dnf install -y libvirt virt-install
+    dnf install -y libvirt virt-install rsync
     systemctl enable libvirtd
     systemctl start libvirtd
 }
@@ -302,7 +305,7 @@ function centos_download {
     CENTOS=/tmp/cvp/centos/centos.iso
     CENTOS_DOWNLOAD=1
     mkdir -p /tmp/cvp/centos
-    if [ -f "/tmp/cvp/centos/centos.iso" ]; then
+    if [ -f "/tmp/cvp/centos/centos.iso" ] && [ ! -n "${ASSUME_YES}" ]; then
         while true; do
             read -p "[DEPLOYER] CentOS image already found. Do you want to re-download? (yes/no) " yn
             case $yn in
@@ -395,6 +398,146 @@ function cloudvision_check {
         echo "[DEPLOYER] Error: Something went wrong. ${LIBVIRT_IMAGES}/cvp.iso is not available." >&2
         exit 1
     fi
+}
+
+# ===========================================================================================
+# == VM related functions
+# ===========================================================================================
+
+function vm_install {
+    virt-install \
+        --virt-type=kvm \
+        --name cvp \
+        --memory=$VM_MEM,maxmemory=$VM_MEM \
+        --cpu host-passthrough \
+        --vcpus=$VM_VCPU --os-variant=rhel7.7 \
+        --location=/var/lib/libvirt/boot/CentOS-7-x86_64-Minimal-1908.iso \
+        --network=bridge=$VM_NET,model=virtio \
+        --disk path=/var/lib/libvirt/images/cvp.root.img,size=$VM_DISKSIZE_ROOT,bus=virtio,format=raw \
+        --disk path=/var/lib/libvirt/images/cvp.data.img,size=$VM_DISKSIZE_DATA,bus=virtio,format=raw \
+        --disk path=/var/lib/libvirt/images/cvp.iso,device=cdrom,bus=sata,readonly=yes \
+        --initrd-inject=/tmp/cvp/ks.cfg \
+        --extra-args "console=tty0 console=ttyS0,115200 rd_NO_PLYMOUTH ks=file:/ks.cfg inst.sshd" \
+        --graphics none \
+        --autostart \
+        --noreboot
+
+    virt-xml cvp --remove-device --disk 3
+    virt-xml cvp --remove-device --disk 3
+    virsh define /etc/libvirt/qemu/cvp.xml
+
+    virsh start cvp
+    virsh console cvp
+}
+
+function vm_generate_ks {
+    touch /tmp/cvp/ks.cfg
+    cat <<EOF > /tmp/cvp/ks.cfg
+# =========================================================================================== > 
+# == Kickstart file for CloudVision VM based on RPM installer and custom specs
+# == Fully automated install
+# == Florian Hibler <florian@arista.com>
+# ===========================================================================================
+# == Root password is 'cvpadmin'
+# ===========================================================================================
+
+# Language settings
+lang en_US.UTF-8
+keyboard --vckeymap=us --xlayouts=''
+timezone Etc/UTC --isUtc --ntpservers=$VM_NTP
+
+# Root password is 'cvpadmin'
+rootpw --iscrypted \$6\$7z9CSB6nxDedcRMy\$WglVJ6UcIlGlIfxjRo/FlyubNA.rewsUKwwqqcjjThK.oahaDGqWRQXS2TARVhmZt95T3Nvig3zvTkiOMdrNr0
+sshpw --username=root --iscrypted \$6\$7z9CSB6nxDedcRMy\$WglVJ6UcIlGlIfxjRo/FlyubNA.rewsUKwwqqcjjThK.oahaDGqWRQXS2TARVhmZt95T3Nvig3zvTkiOMdrNr0 
+
+# Network settings
+network --bootproto=static --device eth0 --ip=$VM_IP --netmask=$VM_NETMASK --gateway=$VM_GW --nameserver=$VM_DNS --noipv6 --activate
+network  --hostname=$VM_FQDN
+
+# Installer settings
+reboot
+cdrom
+text
+
+# Storage and boot configuration
+bootloader --append=" crashkernel=auto" --location=mbr --boot-drive=vda
+zerombr
+clearpart --all --initlabel --drives=vda,vdb
+part /boot --size=512 --ondisk=vda --asprimary --fstype=ext4 --label=boot --fsoptions=acl,user_xattr,errors=remount-ro,nodev,noexec,nosuid
+part pv.00 --size=1 --grow --asprimary --ondisk=vda
+part pv.01 --size=1 --grow --asprimary --ondisk=vdb
+volgroup vg_root pv.00
+volgroup vg_data pv.01
+logvol / --fstype=ext4 --fsoptions=acl,user_xattr,errors=remount-ro --size=1 --grow --name=root --vgname=vg_root
+logvol /data --fstype=ext4 --fsoptions=acl,user_xattr,errors=remount-ro --size=1 --grow --vgname=vg_data
+firstboot --disable
+
+# System settings
+auth --enableshadow --passalgo=sha512
+selinux --disabled
+firewall --enabled --ssh
+services --enabled="chronyd"
+skipx
+
+# Minimal installation
+%packages
+@core
+chrony
+kexec-tools
+-alsa-firmware
+-alsa-tools-firmware
+-aic94xx-firmware
+-iwl100-firmware
+-iwl7265-firmware
+-iwl7260-firmware
+-iwl5150-firmware
+-iwl105-firmware
+-iwl135-firmware
+-iwl4965-firmware
+-iwl6000g2b-firmware
+-iwl6050-firmware
+-ivtv-firmware
+-iwl6000-firmware
+-iwl5000-firmware
+-iwl3945-firmware
+-iwl2030-firmware
+-iwl2000-firmware
+-iwl1000-firmware
+-iwl3160-firmware
+-iwl6000g2a-firmware
+%end
+
+# Disable kdump
+%addon com_redhat_kdump --disable
+%end
+
+# Install CloudVision
+%post --log=/tmp/ks-cvp-rpm-install.log
+mkdir /tmp/cvprpm
+mount /dev/sr1 /tmp/cvprpm
+cd /tmp/cvprpm
+bash ./cvp-rpm-installer* --type demo
+cd / 
+umount /tmp/cvprpm
+%end
+
+# Enforce password policies
+%anaconda
+pwpolicy root --minlen=6 --minquality=1 --notstrict --nochanges --notempty
+pwpolicy user --minlen=6 --minquality=1 --notstrict --nochanges --emptyok
+pwpolicy luks --minlen=6 --minquality=1 --notstrict --nochanges --notempty
+%end
+EOF
+}
+
+function vm_cleanup {
+    virsh destroy cvp
+    virsh undefine cvp
+    rm -Rf /var/lib/libvirt/images/cvp.root.img /var/lib/libvirt/images/cvp.data.img
+}
+
+function cleanup {
+    rm -Rf /tmp/cvp
 }
 
 # ===========================================================================================
